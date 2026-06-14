346cd7ad5f92c2f7fc7f9d525594d0f4d04c4c53
rajritbik <rajritbik@gmail.com>
2026-06-14 11:18:41 +0530

    Initial commit

---
+++ src/constants.js
@@ -0,0 +1,4 @@
+export const URL =
+  "https://generativelanguage.googleapis.com/v1beta/models/gemini-flash-latest:generateContent?key=AIzaSyCZAPIrEvMtv6nFEnGt4AvD0cfjezQ2Q3E";
+  
+// tutorial api   https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=AIzaSyCZAPIrEvMtv6nFEnGt4AvD0cfjezQ2Q3E
