e05de415331c178caa628e5c9aebf28d9b6dca71
rajritbik <rajritbik@gmail.com>
2026-06-14 11:42:40 +0530

    Remove exposed API key completely

--- src/constants.js
+++ src/constants.js
@@ -3,4 +3,3 @@ const API_KEY = import.meta.env.VITE_GEMINI_API_KEY;
 export const URL = 
   `https://generativelanguage.googleapis.com/v1beta/models/gemini-flash-latest:generateContent?key=${API_KEY}`;
   
-// tutorial api   https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=AIzaSyCZAPIrEvMtv6nFEnGt4AvD0cfjezQ2Q3E
