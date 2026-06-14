2437885c81d7f7597f32a83d1584cf6bc7f893a9
rajritbik <rajritbik@gmail.com>
2026-06-14 11:36:16 +0530

    Move API key to env file

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,6 @@
-export const URL =
-  "https://generativelanguage.googleapis.com/v1beta/models/gemini-flash-latest:generateContent?key=AIzaSyCZAPIrEvMtv6nFEnGt4AvD0cfjezQ2Q3E";
+const API_KEY = import.meta.env.VITE_GEMINI_API_KEY;
+
+export const URL = 
+  `https://generativelanguage.googleapis.com/v1beta/models/gemini-flash-latest:generateContent?key=${API_KEY}`;
   
 // tutorial api   https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=AIzaSyCZAPIrEvMtv6nFEnGt4AvD0cfjezQ2Q3E
